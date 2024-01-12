22a0ffdf6801e1e60af40852935f363eb19537a3
ffurkanerdal <ffurkanerdal@gmail.com>
2024-01-12 18:57:56 +0300

    append static files and routes

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const ejs = require('ejs');
+
+// TEMPLATE ENGINE
+
+app.set('view engine', 'ejs');
 
 // MIDDLEWARES
 
 app.use(express.static('public'));
 
+// ROUTES
+
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
 });
+
+app.get('/index.html', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about.html', (req, res) => {
+  res.render('about');
+});
+
+app.get('/post.html',(req, res)=>{
+  res.render('post')
+})
+
+app.get('/add_post.html',(req, res)=>{
+  res.render('add_post')
+})
+
 
 const port = 3000;
 app.listen(port, () => console.log(`http://localhost:${port}`));
