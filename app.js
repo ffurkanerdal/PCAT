747adf5c3001a439b0d9b35e30c84fd528a3e75f
ffurkanerdal <ffurkanerdal@gmail.com>
2024-01-12 18:01:26 +0300

    patika commit

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-const fn = function () {
-  console.log('function working');
-};
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.get('/',(req,res)=>{
+  res.send("Hello Node!");
+})
+
+app.listen(port, () => console.log(`http://localhost:${port}`));
